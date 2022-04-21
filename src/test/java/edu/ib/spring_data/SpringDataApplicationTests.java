e38ab0f581f6a7b5a2c2136f2ae765ae754e901f
TheVosges <79966556+TheVosges@users.noreply.github.com>
2022-04-21 22:16:20 +0200

    Init commit

---
+++ src/test/java/edu/ib/spring_data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ib.spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
